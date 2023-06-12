97d55012bdc7ade1fbbdbbdae397866a84b82f71
beckzod <jumabaevbeckzod@gmail.com>
2023-06-13 01:47:31 +0500

    use to variables

---
+++ variable.php
@@ -0,0 +1,42 @@
+<?php
+/*$number_1 = 34;
+$number_2 = 23; //global variable number
+
+function myTest()
+{
+global $number_1;
+global $number_2;
+
+}
+myTest();
+
+function myTest1()
+{
+$GLOBALS['number_1'] = $GLOBALS['number_2'] + $GLOBALS['number_1'];
+}
+
+myTest1();
+echo $number_1;*/
+
+//static variable
+
+// function myTest2()
+// {
+//     static $number = 56;
+//     echo $number, "</br>";
+//     $number++;
+
+// }
+// myTest2();
+// myTest2();
+// myTest2();
+
+// $result = print "";
+// echo $result;
+
+// $result1=echo "Hello World";//error message
+// echo $result1;
+
+print "Hello World <br>";
+print "Hello World <br> ";
+print "Hello World";
