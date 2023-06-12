3ec285baa8df839ddd75a57ced4b948d753d8ef9
beckzod <jumabaevbeckzod@gmail.com>
2023-06-13 01:46:52 +0500

    first php file

---
+++ hello.php
@@ -0,0 +1,16 @@
+
+<?php
+// *! Variables
+$txt = "Hello World";
+$number1 = 23;
+$number2 = 45.6;
+
+echo $txt;
+echo "<br>";
+echo $number1;
+echo "<br>";
+echo $number2;
+echo "<br>";
+echo $number1 + $number2;
+?>
+
