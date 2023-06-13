ba3f2597f9b31b2d784b5114186bc1e9e775c18f
beckzod <jumabaevbeckzod@gmail.com>
2023-06-13 07:27:14 +0500

    viewed using loopa

---
+++ loops.php
@@ -0,0 +1,55 @@
+<?php
+# loops used to
+
+$count = 0;
+
+// while ($count <= 5) {
+//     echo 'Count: ' . $count, '<br>';
+//     $count++;
+// }
+
+#do ... while method
+
+// do {
+//     $count++;
+//     echo 'Count: ' . $count, '<br>';
+
+// } while ($count < 4);
+
+# for loops
+
+// for ($i = 0; $i < 10; $i++) {
+//     echo 'Count: ' . $i, '<br>';
+// }
+
+#foreach loops
+// $fruits = array('apple', 'banana', 'orange');
+
+// foreach ($fruits as $fruit) {
+//     echo ' ' . $fruit, '<br>';
+// }
+
+// $age = array("Brid" => "33", "Alam" => "20", "Jour" => "25");
+// foreach ($age as $key => $value) {
+//     echo $value . "\n";
+//     # code...
+// }
+
+#break continuous
+
+// for ($i = 1; $i < 15; $i++) {
+//     if ($i == 5) {
+//         continue;
+//     }
+//     echo "Count:$i <br>";
+// }
+// $degree = 1;
+// for ($i = 1; $i <= 700; $i++) {
+//     if ($i % (5 ** $degree) == 0) {
+//         echo "level of five:5^$degree=", $i, "<br>";
+//         $degree++;
+//     } else {
+//         continue;
+
+//     }
+// }
